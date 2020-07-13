8598b40681c58257b9fe5fad2c5112ef119195e0
Shahraiz <45508611+Shahraizz@users.noreply.github.com>
2020-07-13 17:16:42 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const Express = require('express');
+
+const app = Express();
+
+app.get('/', (req, res)=>{
+    res.send('hello world');
+});
+
+app.listen(3000, ()=>{
+    console.log('listening on port 3000');
+});
